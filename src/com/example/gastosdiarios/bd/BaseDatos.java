6aecc6a225ea51c9f2d1d0d4305e3abe1fa2dd03
JENNIFER ALEXIA KARCEVAS <38050235@ort>
2017-11-14 19:43:12 -0300

    arreglo create db

--- src/com/example/gastosdiarios/bd/BaseDatos.java
+++ src/com/example/gastosdiarios/bd/BaseDatos.java
@@ -8,7 +8,7 @@ import com.example.gastordiarios.model.*;
 
 public class BaseDatos extends SQLiteOpenHelper {
 
-	private String sqlCreateGastos="CREATE TABLE" + GastosContract.TableGastos.TABLE_NAME
+	private String sqlCreateGastos="CREATE TABLE IF NOT EXISTS " + GastosContract.TableGastos.TABLE_NAME
 			+"("+GastosContract.TableGastos.COLUMN_NAME__ID+" INTEGER PRIMARY KEY AUTOINCREMENT, "
 			+ GastosContract.TableGastos.COLUMN_NAME_ID_CATEGORIA+" INTEGER, "
 			+ GastosContract.TableGastos.COLUMN_NAME_DESCRIPCION+" TEXT, "
@@ -17,9 +17,9 @@ public class BaseDatos extends SQLiteOpenHelper {
 			+"FOREIGN KEY("+GastosContract.TableGastos.COLUMN_NAME_ID_CATEGORIA+") REFERENCES "
 			+CategoriasContract.TableCategoria.TABLE_NAME+"("+CategoriasContract.TableCategoria.COLUMN_NAME__ID+"))";
 	
-	private String sqlCreateCategorias="CREATE TABLE" + GastosContract.TableGastos.TABLE_NAME
+	private String sqlCreateCategorias="CREATE TABLE IF NOT EXISTS " + GastosContract.TableGastos.TABLE_NAME
 			+"("+GastosContract.TableGastos.COLUMN_NAME__ID+" INTEGER PRIMARY KEY AUTOINCREMENT,"
-			+ GastosContract.TableGastos.COLUMN_NAME_DESCRIPCION+" INTEGER";
+			+ GastosContract.TableGastos.COLUMN_NAME_DESCRIPCION+" INTEGER)";
 	
 	public BaseDatos(Context gastosManejador, String name, CursorFactory factory, int version) {
 		super(gastosManejador, name, factory, version);
